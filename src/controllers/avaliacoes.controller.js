a7d1ba5829a8943ddc571a1f4b85db0e6d7674bf
Nikollas Dheyvis <nikollas.dheyvis77@gmail.com>
2025-11-05 09:36:55 -0300

    Backend inicial finalizado

--- src/controllers/avaliacoes.controller.js
+++ src/controllers/avaliacoes.controller.js
@@ -2,19 +2,42 @@ import { db } from "../config/db.js";
 
 export async function criarAvaliacao(req, res) {
     try {
-        const { usuario_id, livro_id, nota } = req.body;
+        const { usuario_id, livro_id, nota, comentario } = req.body;
         
         const notaDecimal = Number.parseFloat(Number(nota).toFixed(1))
         if (!usuario_id || !livro_id || !nota) return res.status(400).json({ erro: "Preencha os campos obrigatórios" });
 
-        await db.execute(
-            "INSERT INTO avaliacoes (usuario_id, livro_id, nota) VALUES (?, ?, ?)",
-            [usuario_id, livro_id, notaDecimal]
+        if(nota >10 || nota <0){
+            return res.status(400).json({erro:"Por favor, a nota deve ser maior que 0 e menor que 10."})
+        }
+        const [resultado] = await db.execute(
+            "SELECT * from avaliacoes WHERE usuario_id = ? AND livro_id = ?",
+            [usuario_id, livro_id]
         );
 
-        res.json({ mensagem: "Avaliação criada com sucesso!" });
+        console.log(resultado)
+        if(resultado.length > 0){
+            return res.status(200).json({msg:"Usuario ja fez avaliação ."})
+        }
+        const [inserir] = await db.execute(
+            "INSERT INTO avaliacoes (usuario_id, livro_id, nota, comentario) VALUES (?, ?, ?, ?)",
+            [usuario_id, livro_id, notaDecimal, comentario]
+        );
+
+        console.log(inserir)
+
+        if(inserir.affectedRows >= 1){
+             res.status(200).json({ mensagem: "Avaliação criada com sucesso!" ,
+                id: inserir.insertId
+             });
+        }else{
+            res.status(500).json({ erro: err.message });
+        }
+
+       
     } catch (err) {
         res.status(500).json({ erro: err.message });
+        res.send("Erro ao solicitar requisição")
     }
 };
 
